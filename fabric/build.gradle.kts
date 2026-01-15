aa3ac30168a6a74f1966bd8818b5411637f8eb72
Bruno Paré-simard <bruno.paresimard@gmail.com>
2026-01-15 13:25:45 -0500

    created the specific build script based on the architecture template
    - TODO: Reintegrate the releasing tasks
    - TODO: Reintegrate the `modLocalRuntime` dependency for development if needed. Left out until validation if Fabric specific or both.
    - TODO: Reintegrate the task `generateStandaloneRun`. Left out until validation if Fabric specific or both.
    - for fabric, using the main root fabric-api dependency instead of the multiple child one.
    - removed the mixin configuration as no mixin are used.
    - only added dependencies called in the code, such as `viafabricplus` being left out, keeping only `viaversion`. this needs to be validated.
    - removed the loom client config; it did not seems to affect the build. this needs to be validated.

---
+++ fabric/build.gradle.kts
@@ -0,0 +1,66 @@
+repositories {
+    maven { setUrl("https://maven.terraformersmc.com/") }
+    maven { setUrl("https://repo.viaversion.com") }
+}
+
+plugins {
+    alias(libs.plugins.shadow)
+}
+
+architectury {
+    platformSetupLoomIde()
+    fabric()
+}
+
+val common: Configuration by configurations.creating
+val shadowBundle: Configuration by configurations.creating
+val developmentFabric: Configuration by configurations.getting
+
+configurations {
+    common.isCanBeResolved = true
+    common.isCanBeConsumed = false
+
+    compileClasspath {
+        extendsFrom(common)
+    }
+    runtimeClasspath {
+        extendsFrom(common)
+    }
+    developmentFabric.extendsFrom(common)
+
+    // Files in this configuration will be bundled into your mod using the Shadow plugin.
+    // Don't use the `shadow` configuration from the plugin itself as it's meant for excluding files.
+    shadowBundle.isCanBeResolved = true
+    shadowBundle.isCanBeConsumed = false
+}
+
+dependencies {
+    modImplementation(rootProject.libs.fabric.loader)
+    modImplementation(rootProject.libs.fabric.api)
+
+    common(project(":common", "namedElements")) { isTransitive = false }
+    shadowBundle(project(":common", "transformProductionFabric"))
+
+    modImplementation(rootProject.libs.cuiProtocol.fabric)
+    modImplementation(rootProject.libs.modmenu)
+    modCompileOnly(rootProject.libs.viaversion)
+}
+
+tasks.processResources {
+    inputs.property("version", project.version)
+
+    filesMatching("fabric.mod.json") {
+        expand("version" to project.version)
+    }
+}
+
+tasks.shadowJar {
+    configurations = listOf(shadowBundle)
+    archiveClassifier.set("dev-shadow")
+}
+
+tasks.remapJar {
+    inputFile.set(tasks.shadowJar.get().archiveFile)
+}
+
+// TODO: Add the releasing build scripts
